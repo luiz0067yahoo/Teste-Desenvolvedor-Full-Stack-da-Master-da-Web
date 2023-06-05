8475af817ac94973cc2c6cc5515bacb75d990b4e
luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2023-06-05 16:58:33 -0300

    start front end

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+    Login
 </template>
 
 <style scoped>
-
-
 </style>
--- teste/vue-project/src/main.ts
+++ teste/vue-project/src/main.ts
@@ -1,5 +1,4 @@
-import './assets/css/style.css'
-import './assets/sass/style.scss'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
